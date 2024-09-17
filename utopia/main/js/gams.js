969225b6042f0a179cf0657b349641119869dc2a
Fox Woodbury <foxwoodbury@gmail.com>
2024-09-17 11:46:19 +0100

    Realised Utopia *is* in fact not a static page - coming back to it soon!

--- utopia/main/js/gams.js
+++
@@ -1,3 +0,0 @@
-if(location.hash != ''){
-    document.querySelector(".gam").src = window.location.origin + location.hash.substring(1);
-}
--- utopia/main/js/terms.js
+++
@@ -1,1 +0,0 @@
-document.querySelector(".url").innerText = window.location.origin;
